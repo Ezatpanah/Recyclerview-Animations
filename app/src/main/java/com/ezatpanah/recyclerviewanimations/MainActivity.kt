8d01148d6e3791f1df261f841b05c6464a790347
rezatpanah <Ezatpanah@gmail.com>
2022-01-07 15:24:55 +0200

    update code

--- app/src/main/java/com/ezatpanah/recyclerviewanimations/MainActivity.kt
+++ app/src/main/java/com/ezatpanah/recyclerviewanimations/MainActivity.kt
@@ -2,10 +2,58 @@ package com.ezatpanah.recyclerviewanimations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ezatpanah.recyclerviewanimations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var nameList : MutableList<SampleModel> = mutableListOf()
+    private lateinit var sampleAdapter: SampleAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadData()
+
+
+        sampleAdapter= SampleAdapter(nameList)
+        binding.apply {
+            recyclerView.apply {
+                layoutManager=LinearLayoutManager(this@MainActivity)
+                adapter=sampleAdapter
+            }
+        }
+
+
+    }
+
+
+    private fun loadData(){
+        nameList.add(SampleModel(1,"Reyhaneh"))
+        nameList.add(SampleModel(2,"Emad"))
+        nameList.add(SampleModel(3,"zahra"))
+        nameList.add(SampleModel(4,"pejman"))
+        nameList.add(SampleModel(5,"sara"))
+        nameList.add(SampleModel(6,"nariman"))
+        nameList.add(SampleModel(7,"hamide"))
+        nameList.add(SampleModel(8,"farshad"))
+        nameList.add(SampleModel(9,"simin"))
+        nameList.add(SampleModel(10,"ali"))
+        nameList.add(SampleModel(11,"fateme"))
+        nameList.add(SampleModel(12,"reza"))
+        nameList.add(SampleModel(13,"Reyhaneh"))
+        nameList.add(SampleModel(14,"Emad"))
+        nameList.add(SampleModel(15,"zahra"))
+        nameList.add(SampleModel(16,"pejman"))
+        nameList.add(SampleModel(17,"sara"))
+        nameList.add(SampleModel(18,"nariman"))
+        nameList.add(SampleModel(19,"hamide"))
+        nameList.add(SampleModel(20,"farshad"))
+        nameList.add(SampleModel(21,"simin"))
+        nameList.add(SampleModel(22,"ali"))
+        nameList.add(SampleModel(23,"fateme"))
+        nameList.add(SampleModel(24,"reza"))
     }
 }
--- app/src/main/java/com/ezatpanah/recyclerviewanimations/SampleAdapter.kt
+++ app/src/main/java/com/ezatpanah/recyclerviewanimations/SampleAdapter.kt
@@ -1,5 +1,6 @@
 package com.ezatpanah.recyclerviewanimations
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -9,18 +10,22 @@ class SampleAdapter(private val items : MutableList<SampleModel>) : RecyclerView
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(inflater.inflate(R.layout.item_row,parent,false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       holder.bind(items[position])
     }
 
     override fun getItemCount() = items.size
 
-    class ViewHolder(menuView : View) : RecyclerView.ViewHolder(menuView) {
-
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        private  val tvId= itemView.findViewById<TextView>(R.id.tvId)
+        private  val tvName= itemView.findViewById<TextView>(R.id.tvName)
         fun bind(item : SampleModel){
+            tvId.text=item.id.toString()
+            tvName.text=item.name
 
         }
 
